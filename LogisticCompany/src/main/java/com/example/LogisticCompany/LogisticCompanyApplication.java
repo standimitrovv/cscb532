b736f46a89d3203a5fa4c39a7378a16b769ea66e
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-05 15:39:47 +0200

    "setup Spring Boot app"

---
+++ LogisticCompany/src/main/java/com/example/LogisticCompany/LogisticCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogisticCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticCompanyApplication.class, args);
+	}
+
+}
---
+++ LogisticCompany/src/test/java/com/example/LogisticCompany/LogisticCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogisticCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
