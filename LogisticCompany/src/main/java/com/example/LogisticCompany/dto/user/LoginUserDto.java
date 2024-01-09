f90a47564462b8ade180de949393582ee5c752b9
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-09 16:29:03 +0200

    refactor: add a leftout class annotation

--- LogisticCompany/src/main/java/com/example/LogisticCompany/dto/user/LoginUserDto.java
+++ LogisticCompany/src/main/java/com/example/LogisticCompany/dto/user/LoginUserDto.java
@@ -1,9 +1,11 @@
 package com.example.LogisticCompany.dto.user;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class LoginUserDto extends UserDto {
 }
