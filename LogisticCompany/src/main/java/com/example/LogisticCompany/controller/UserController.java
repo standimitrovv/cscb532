52f32f3d0665a9b42c09e377dc8fa26a06ae6364
Stanimir Dimitrov <evlution63@gmail.com>
2024-01-09 12:21:53 +0200

    refactor: add AUtowired annotation

--- LogisticCompany/src/main/java/com/example/LogisticCompany/controller/UserController.java
+++ LogisticCompany/src/main/java/com/example/LogisticCompany/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.LogisticCompany.dto.user.RegisterUserDto;
 import com.example.LogisticCompany.dto.user.UserDtoResponse;
 import com.example.LogisticCompany.service.implementation.UserServiceImpl;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserServiceImpl userService;
 
+    @Autowired
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
--- LogisticCompany/src/main/java/com/example/LogisticCompany/service/implementation/UserServiceImpl.java
+++ LogisticCompany/src/main/java/com/example/LogisticCompany/service/implementation/UserServiceImpl.java
@@ -5,12 +5,14 @@ import com.example.LogisticCompany.dto.user.RegisterUserDto;
 import com.example.LogisticCompany.dto.user.UserDtoResponse;
 import com.example.LogisticCompany.repository.UserRepository;
 import com.example.LogisticCompany.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
